ba6f8796f2d9fee8e09d47081494ead24e86ae08
roudYStorm <anzhelikaroud@yandex.ru>
2023-12-20 11:15:07 +0300

    Counter basic task

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ButtonClick: UIButton!
+    @IBOutlet weak var LabelCount: UILabel!
+    var counter = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        LabelCount.text = "0"
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func PlusNumber(_ sender: Any) {
+        counter += 1
+       
+            LabelCount.text = "Значение счетчика: \(counter)"
+        LabelCount.sizeToFit()
+        LabelCount.center = self.view.center
+    }
+    
 }
 
